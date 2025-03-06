71ef17be5618aa7fa2df5372d6c4df8e8e9ce7ac
thaichihien <thaichihien@gmail.com>
2025-03-06 10:56:06 +0700

    remove old admin

--- admin/src/interfaces/size.ts
+++
@@ -1,4 +0,0 @@
-export interface Size{
-    id: string
-    name: string
-}
--- admin/src/interfaces/user-profile.ts
+++
@@ -1,8 +0,0 @@
-
-export interface UserProfile{
-    id: string;
-    email: string;
-    firstName: string;
-    lastName: string;
-    roles: string[];
-}
